41b5d242b639aa11ccfd1ff966998a607724c237
Diego Guerrero (Yogui) <dguerrero875@gmail.com>
2023-02-03 14:06:29 -0800

    Merge two sorted lists
    problem from leetcode merge two sorted lists

---
+++ mergetwosortedlists.py
@@ -0,0 +1,15 @@
+#from leetcode problem merge two sorted lists
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        newList = ListNode(0)
+        cur = newList
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+        cur.next = list1 or list2
+        return newList.next
