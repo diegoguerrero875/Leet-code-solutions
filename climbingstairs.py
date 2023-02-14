628362d7a90cf4c71bb6b496ae7f04f2ea0ec20e
Yogui <dguerrero875@gmail.com>
2023-02-13 16:46:28 -0800

    from leetcode climbing stairs problem

---
+++ climbingstairs.py
@@ -0,0 +1,15 @@
+#from leetcode climbing the stairs problem
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 2
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n - 1]
